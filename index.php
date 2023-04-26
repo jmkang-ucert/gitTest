467482163d1997a73cbc373fdf8d94b7f5fb7fa3
Kang Jimin <jmkang@ucert.co.kr>
2023-04-26 19:20:16 +0900

    test

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
   // echo "PR 테스트";
 
-  // phpinfo();
-
+  // phpinfo();  
 
   echo "dev에만 커밋";
   
@@ -11,3 +10,5 @@
   echo "dev에만 커밋 수정ㄴㄴㄴㄴㄴ222222";
   echo "dev에만 커밋 수정ㄴㄴㄴㄴㄴ444444";
   echo "dev에만 커밋 수정ㄴㄴㄴㄴㄴ444444 testtesttesttest";
+
+  echo "tetetete";
